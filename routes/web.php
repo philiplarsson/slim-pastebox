ff15fe3ab316d7d7d1a510601576182d39209958
Philip Larsson <elt11pla@student.lu.se>
2017-07-30 04:47:26 +0200

    Add navigation

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 $app->get('/', function (Request $request, Response $response) {
     return $this->view->render($response, 'home.twig');
-});
+})->setName('home');
 
-$app->get('/pastes', PasteController::class . ':showPastes');
+$app->get('/pastes', PasteController::class . ':showPastes')->setName('showPastes');
 $app->get('/p/{base62}', PasteController::class . ':showPaste');
