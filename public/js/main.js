6f51616c33334ab5d1cce954a2a33ec788266b58
Philip Larsson <elt11pla@student.lu.se>
2017-07-30 18:39:15 +0200

    Add jquery and close notification div

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+// Hide notification when pressing delete button
+$(document).ready(function() {
+    $('button.delete').click(function() {
+       $(this).closest('.notification').hide();
+    });
+});
